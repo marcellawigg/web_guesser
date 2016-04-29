8811e5d1007f58240b6337c0be4e15bc81215603
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-04-29 11:41:04 -0600

    Iteration 3 first part

--- web_guesser/web_guesser.rb
+++ web_guesser/web_guesser.rb
@@ -2,8 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :port, 8888
-number = rand(101)
+number ||= rand(101)
+
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  response = check(guess, number)
+  erb :index, :locals => {:number => number, :guess => guess, :response => response}
 end
+
+
+  def check(guess, number)
+    guess = guess.to_i unless guess.nil?
+    if guess.nil?
+      "Make a guess"
+    elsif guess > number
+      "Too high!"
+    elsif guess < number
+      "Too low!"
+    elsif guess == number
+      "You got it right!"
+    end
+  end
